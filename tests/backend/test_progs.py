9ff62973f794fced28183aecb52902eccc2bd710
Filippo Carra <karrukola@users.noreply.github.com>
2019-09-04 23:20:36 +0200

    Marked previous test as webtest

--- tests/backend/test_progs.py
+++ tests/backend/test_progs.py
@@ -1,8 +1,10 @@
+import pytest
 from resources.lib.deejay_it_parser import DeejayItParser
 from .support.assertions import assert_valid_schema
 
 
-def test_programs():
+@pytest.mark.webtest
+def test_get_programs():
     deejay = DeejayItParser()
     json_data = deejay.q_programs()
     assert_valid_schema(json_data, 'programs.json')
