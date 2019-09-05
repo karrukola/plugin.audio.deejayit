5befa4d9480b38e6159906f2b005d095dbe88d5b
Filippo Carra <karrukola@users.noreply.github.com>
2019-09-05 22:44:33 +0200

    Refactoring assertions to load json files from a folder that is not schema

--- tests/webtest/support/assertions.py
+++ tests/webtest/support/assertions.py
@@ -8,8 +8,16 @@ def assert_valid_schema(data, schema_file):
     return validate(data, schema)
 
 
+def assert_expected_answer(data, exp_answ):
+    return data == _load_json_file(exp_answ)
+
+
 def _load_json_schema(filename):
     rel_path = join('schemas', filename)
-    abs_path = join(dirname(__file__), rel_path)
+    return _load_json_file(rel_path)
+
+
+def _load_json_file(filename):
+    abs_path = join(dirname(__file__), filename)
     with open(abs_path) as schema_file:
         return loads(schema_file.read())
