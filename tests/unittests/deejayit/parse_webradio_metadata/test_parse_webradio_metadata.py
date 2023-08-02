b4ac63c0e93724e542a606c21bf963046286cba1
Filippo Carra <karrukola@users.noreply.github.com>
2023-08-02 15:23:30 +0200

    refactor(test_parse_webradio_metadata): address linting issues by Ruff
    Signed-off-by: Filippo Carra <karrukola@users.noreply.github.com>

--- tests/unittests/deejayit/parse_webradio_metadata/test_parse_webradio_metadata.py
+++ tests/unittests/deejayit/parse_webradio_metadata/test_parse_webradio_metadata.py
@@ -1,12 +1,25 @@
+"""Unit tests for the function parsing metadata of a live webradio.
+
+Data is fed via mocked HTTP responses.
+"""
 from pathlib import Path
 
 from resources.lib.deejayit import DeejayIt, WebRadioMetadata
 
-LINETTI_URL = "https://streamcdnb10-4c4b867c89244861ac216426883d1ad0.msvdn.net/webradio/metadata/deejaywfmlinus.json"
-DEEJAY_TIME_URL = "https://streamcdnb2-4c4b867c89244861ac216426883d1ad0.msvdn.net/webradio/metadata/deejaytime.json"
+# ruff: noqa: INP001, S101, B011, ANN201, ANN001
+
+LINETTI_URL = "https://streamcdnb10-4c4b867c89244861ac216426883d1ad0.msvdn.net/webradio/metadata/deejaywfmlinus.json"  # noqa: E501
+DEEJAY_TIME_URL = "https://streamcdnb2-4c4b867c89244861ac216426883d1ad0.msvdn.net/webradio/metadata/deejaytime.json"  # noqa: E501
 
 
 def test_linetti(requests_mock):
+    """Parse information for Radio Linetti.
+
+    This is a positive case, where all information is populated by the API.
+
+    :param requests_mock: mocker for requests
+    :type requests_mock: mocked request
+    """
     mock_answer = Path(__file__).parent / "linetti.json"
     requests_mock.get(LINETTI_URL, text=mock_answer.read_text())
     out = DeejayIt.parse_webradio_metadata(LINETTI_URL)
@@ -16,7 +29,7 @@ def test_linetti(requests_mock):
         "The Look Of Love",
         "ABC",
         "The Lexicon Of Love",
-        "https://4c4b867c89244861ac216426883d1ad0.msvdn.net/radio/coverart/song003275_100x100.jpg",
+        "https://4c4b867c89244861ac216426883d1ad0.msvdn.net/radio/coverart/song003275_100x100.jpg",  # noqa: E501
         "2023-07-22 11:46:25",
     )
 
@@ -24,6 +37,13 @@ def test_linetti(requests_mock):
 
 
 def test_deejaytime_no_album(requests_mock):
+    """Parse information when no album is given.
+
+    This is a case found first for Deejay Time, the album information is empty.
+
+    :param requests_mock: mocker for requests
+    :type requests_mock: mocked request
+    """
     mock_answer = Path(__file__).parent / "deejay_time.json"
     requests_mock.get(DEEJAY_TIME_URL, text=mock_answer.read_text())
     out = DeejayIt.parse_webradio_metadata(DEEJAY_TIME_URL)
@@ -33,7 +53,7 @@ def test_deejaytime_no_album(requests_mock):
         "All I Really Want",
         "KIM LUKAS",
         None,
-        "https://4c4b867c89244861ac216426883d1ad0.msvdn.net/radio/coverart/song078194_100x100.jpg",
+        "https://4c4b867c89244861ac216426883d1ad0.msvdn.net/radio/coverart/song078194_100x100.jpg",  # noqa: E501
         "2023-07-22 23:05:24",
     )
 
